17bbd0dc40433af8b3d7852fc98c249d9e940f64
autbru2023tk23 <sabisabitha308@gmail.com>
2025-07-26 01:19:24 -0700

    Add files via upload

---
+++ ATM.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+int choice;
+float balance=1000.00,amount;
+
+while(1){
+        printf("1. Balance\n");
+        printf("2.deposit money \n");
+        printf("3.whithdraw money \n");
+        printf("4.exit \n");
+
+        printf("enter a choice:");
+        scanf("%d,&choice");
+        switch (choice){
+        case 1:
+            print
